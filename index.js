50571d0121ad8ca7d73e95d066185ba6a2d552c8
Irene <ivinentmercadal@iesjoanramis.org>
2025-05-07 12:03:49 +0200

    primera sessió firebase

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 import { saveDespesa } from './firebase.js'
 
-window.addEventListener("DOMContentLoaded", ()=>{
-    saveDespesa({
-        concepte: "Sopar", 
-        quantia: "34,5",
-        pagatPer: "Joan"
+const despesesForm = document.getElementById("despeses-form");
+
+function addEventListenerToSaveDespesa () {
+    despesesForm.addEventListener("submit", (e)=>{
+        e.preventDefault();
+
+        const concepte = despesesForm["despesa-concepte"].value;
+        const quantia = despesesForm["despesa-quantia"].value;
+        const pagatPer = despesesForm["despesa-pagat-per"].value;
+
+        const despesa = { concepte, quantia, pagatPer};
+
+        console.log(despesa);
+
+        saveDespesa(despesa);
     });
+}
+
+
+window.addEventListener("DOMContentLoaded", ()=>{
+    // saveDespesa({
+    //     concepte: "Sopar", 
+    //     quantia: "34,5",
+    //     pagatPer: "Joan"
+    // });
+
+    addEventListenerToSaveDespesa();
+
 });
